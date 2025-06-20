d8db168854d0b860e0692eae6230dbf0d1644e26
Prashant Patil <prashant1001001@gmail.com>
2025-06-20 11:49:59 +0530

    connected to mongodb atlas

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.connect('mongodb://0.0.0.0/Productivity-Tracker')
+const connection = mongoose.connect('mongodb+srv://Prash1001:pass123@timemanagment.2xc0ylj.mongodb.net/?retryWrites=true&w=majority&appName=TimeManagment')
 .then(() => console.log("conneted to database"))
 .catch((err) => console.error("Error in connection to database", err));
 
