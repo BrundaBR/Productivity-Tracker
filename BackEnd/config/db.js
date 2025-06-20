2a2ad711c6f669be2de8f0dd5dbc35b9bcfa3643
Prashant Patil <prashant1001001@gmail.com>
2025-06-20 12:30:15 +0530

    Adding .env file

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-const connection = mongoose.connect('mongodb+srv://Prash1001:pass123@timemanagment.2xc0ylj.mongodb.net/?retryWrites=true&w=majority&appName=TimeManagment')
+const mongoUrl = process.env.MONGODB_URL;
+
+const connection = mongoose.connect(mongoUrl)
 .then(() => console.log("conneted to database"))
 .catch((err) => console.error("Error in connection to database", err));
 
